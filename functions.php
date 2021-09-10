5987d19a810b9d06d9cd610b16dd7c163f93e709
Chris L <chris.ladden@gmail.com>
2021-09-10 12:17:52 -0500

    Bumps theme version to 0.0.3

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-  define( 'THEME_VERSION', '0.0.2' );
+  define( 'THEME_VERSION', '0.0.3' );
 
   /**
    * Load acf
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------------*/
 // Theme information (name, starting theme version)
 var theme        = 'Chris-Ladden',
-    version      = '0.0.2'; // updated with gulp version task, do not edit here.
+    version      = '0.0.3'; // updated with gulp version task, do not edit here.
 
 // Set the paths you will be working with
 var phpFiles     = ['./**/*.php', './*.php'],
